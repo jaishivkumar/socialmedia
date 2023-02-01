5f38d1dceaeb741f242a861d0201fada11f18aa9
jaishivkumar <106514172+jaishivkumar@users.noreply.github.com>
2023-02-01 23:03:02 +0530

    Delete j.js

--- m.js/j.js
+++
@@ -1,7 +0,0 @@
-// // Case: Warehouse owners have warehouses full of book collections. 
-// People want to visit the warehouse closest to their locations and read books
-//  they're interested in. As a backend developer write a simple api using express 
-//  and mongoDB to
-
-// 1. Design a DB schema for this problem
-// 2. fetch warehouses closest to their locations
